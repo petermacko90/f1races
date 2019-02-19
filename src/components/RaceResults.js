496bda6f6ecb93dad228b2c94839cb4c057c18b5
Peter Macko <petermacko90@gmail.com>
2019-02-19 22:44:24 +0100

    add data to results

--- src/components/RaceResults.js
+++ src/components/RaceResults.js
@@ -7,10 +7,15 @@ const RaceResults = ({ results }) => {
       <table>
         <thead>
           <tr>
-            <th className='position'>Pos.</th>
+            <th className='tr' title='Finish Position'>Pos.</th>
+            <th className='tr' title='Grid Position'>Grid</th>
             <th>Driver</th>
             <th>Constructor</th>
             <th>Time</th>
+            <th className='tr'>Laps</th>
+            <th>Status</th>
+            <th>Fastest Lap</th>
+            <th title='Average Speed'>Avg Speed</th>
           </tr>
         </thead>
         <tbody>
@@ -18,10 +23,21 @@ const RaceResults = ({ results }) => {
             results.map(res => {
               return (
                 <tr key={res.position}>
-                  <td className='position'>{res.position}.</td>
+                  <td className='tr'>{res.position}.</td>
+                  <td className='tr'>{res.grid}.</td>
                   <td>{res.Driver.givenName} {res.Driver.familyName}</td>
                   <td>{res.Constructor.name}</td>
-                  <td>{res.Time ? res.Time.time : res.status}</td>
+                  <td>{res.Time ? res.Time.time : 'N/A'}</td>
+                  <td className='tr'>{res.laps}</td>
+                  <td>{res.status}</td>
+                  <td>{res.FastestLap ? res.FastestLap.Time.time: 'N/A'}</td>
+                  <td>
+                    {
+                      res.FastestLap ?
+                        `${res.FastestLap.AverageSpeed.speed} ${res.FastestLap.AverageSpeed.units}`
+                      : 'N/A'
+                    }
+                  </td>
                 </tr>
               );
             })
