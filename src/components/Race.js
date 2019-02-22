fe4f72da5c11e3eac902ee7afc707a179e6ef3c6
Peter Macko <petermacko90@gmail.com>
2019-02-22 23:06:42 +0100

    remove proptypes 2

--- src/components/Race.js
+++ src/components/Race.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import LocalDate from './LocalDate';
 import LocalTime from './LocalTime';
 
--- src/components/RaceList.js
+++ src/components/RaceList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Race from './Race';
 
 const RaceList = ({ races, upcomingRace, onClickRace, onEnterRace }) => {
