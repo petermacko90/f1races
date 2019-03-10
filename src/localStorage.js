7449d30037726d7e47e5792f1a916f2c1096f4d5
Peter Macko <petermacko90@gmail.com>
2019-03-10 15:30:20 +0100

    add validation for saved notifications

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,3 +1,5 @@
+import Joi from 'joi';
+
 export const saveRaces = (races, season) => {
   try {
     const serializedRaces = JSON.stringify(races);
@@ -29,15 +31,32 @@ export const saveNotifications = (notifications) => {
 
 export const loadNotifications = () => {
   try {
+    const notificationObject = Joi.object().keys({
+      body: Joi.string().required(),
+      raceDate: Joi.date().required(),
+      notificationDate: Joi.date().required(),
+      notified: Joi.boolean().required(),
+      title: Joi.string().required()
+    });
+    const schema = Joi.array().items(notificationObject);
+
     const serializedNotifications = localStorage.getItem('notifications');
     if (serializedNotifications === null) return undefined;
-    return JSON.parse(serializedNotifications, (key, value) => {
+    const notifications = JSON.parse(serializedNotifications, (key, value) => {
       if (key === 'notificationDate' || key === 'raceDate') {
         return new Date(value);
       } else {
         return value;
       }
     });
+    
+    const test = Joi.validate(notifications, schema);
+    if (test.error) {
+      localStorage.removeItem('notifications');
+      return undefined;
+    } else {
+      return notifications;
+    }
   } catch (error) {
     return undefined;
   }
