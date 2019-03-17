b03dcbf5ba016b13f7c5de2e84b2939b07b8b137
Peter Macko <petermacko90@gmail.com>
2019-03-17 11:27:36 +0100

    updated Notifications component

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -4,51 +4,54 @@ const Notifications = ({ notifications, deleteNotification }) => {
   const sortedNotifications = notifications.sort((a, b) => {
     return a.notificationDate - b.notificationDate;
   });
-  if (notifications.length === 0) {
-    return <p className='p10'>No saved notifications</p>;
-  } else {
-    return (
-      <div className='container'>
-        <div className='responsive-table'>
-          <h2 className='ml10 mr10'>Saved Notifications</h2>
-          <table>
-            <thead>
-              <tr>
-                <th></th>
-                <th>Title</th>
-                <th>Notified</th>
-                <th>Race Date</th>
-                <th>Notification Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                sortedNotifications.map(notification => {
-                  const {
-                    id, title, notified, raceDate, notificationDate
-                  } = notification;
-                  return (
-                    <tr key={id}>
-                      <td>
-                        <button onClick={deleteNotification(id)}
-                        className='button' title='Delete Notification'>
-                          &times;
-                        </button>
-                      </td>
-                      <td>{title}</td>
-                      <td>{notified ? 'Yes': 'No'}</td>
-                      <td>{raceDate.toLocaleDateString()} {raceDate.toLocaleTimeString()}</td>
-                      <td>{notificationDate.toLocaleDateString()} {notificationDate.toLocaleTimeString()}</td>
-                    </tr>
-                  );
-                })
-              }
-            </tbody>
-          </table>
-        </div>
-      </div>
-    );
-  }
+
+  return (
+    <div className='container'>
+      <h2 className='ml10 mr10'>Saved Notifications</h2>
+      {
+        notifications.length === 0 ?
+          <p className='p10'>No saved notifications</p>
+        :
+          <div className='responsive-table'>
+            <table>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Title</th>
+                  <th>Notified</th>
+                  <th>Race Date</th>
+                  <th>Notification Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  sortedNotifications.map(notification => {
+                    const {
+                      id, title, notified, raceDate, notificationDate
+                    } = notification;
+                    return (
+                      <tr key={id}>
+                        <td>
+                          <button onClick={deleteNotification(id)}
+                          className='button' title='Delete Notification'>
+                            &times;
+                          </button>
+                        </td>
+                        <td>{title}</td>
+                        <td>{notified ? 'Yes': 'No'}</td>
+                        <td>{raceDate.toLocaleDateString()} {raceDate.toLocaleTimeString()}</td>
+                        <td>{notificationDate.toLocaleDateString()} {notificationDate.toLocaleTimeString()}</td>
+                      </tr>
+                    );
+                  })
+                }
+              </tbody>
+            </table>
+          </div>
+      }
+    </div>
+  );
+  
 }
 
 export default Notifications;
