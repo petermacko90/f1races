d16142eaf9d63a870f64d4124bf62250a2e680d6
Peter Macko <petermacko90@gmail.com>
2019-03-29 23:35:07 +0100

    display buttons in SeasonSelect conditionally

--- src/components/SeasonSelect.js
+++ src/components/SeasonSelect.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { FIRST_SEASON } from '../constants';
+import { FIRST_SEASON, CURRENT_SEASON } from '../constants';
 import { ThemeConsumer } from '../ThemeContext';
 
 const SeasonSelect = ({ season, onSelectSeason, onChangeSeason }) => {
   let seasonOptions = [];
-  for (let i = FIRST_SEASON, d = new Date().getFullYear(); i <= d; i++) {
+  for (let i = FIRST_SEASON; i <= CURRENT_SEASON; i++) {
     seasonOptions.push(<option key={i} value={i}>Season {i}</option>);
   }
 
@@ -12,18 +12,22 @@ const SeasonSelect = ({ season, onSelectSeason, onChangeSeason }) => {
     <ThemeConsumer>
       {theme =>
         <div className='season ml10 mb10'>
-          <button onClick={onChangeSeason(-1)} title='Previous season'
-          className={'button ' + theme}>
-            &lt;
-          </button>
+          { season !== FIRST_SEASON &&
+            <button onClick={onChangeSeason(-1)} title='Previous season'
+            className={'button ' + theme}>
+              &lt;
+            </button>
+          }
           <select value={season} onChange={onSelectSeason}
           className={'p10 season-select ' + theme} aria-label='Select season'>
             {seasonOptions}
           </select>
-          <button onClick={onChangeSeason(1)} title='Next season'
-          className={'button ' + theme}>
-            &gt;
-          </button>
+          { season !== CURRENT_SEASON &&
+            <button onClick={onChangeSeason(1)} title='Next season'
+            className={'button ' + theme}>
+              &gt;
+            </button>
+          }
         </div>
       }
     </ThemeConsumer>
