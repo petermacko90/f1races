616d21e470a696e0b0f0dcfb0aa7566439583b0e
Peter Macko <petermacko90@gmail.com>
2019-04-07 20:53:01 +0200

    Updates

---
+++ precache-manifest.e329ae9aab29b53b85744751681d73f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "91779dc48555e54248ab",
+    "url": "/f1races/static/js/runtime~main.d1915ec3.js"
+  },
+  {
+    "revision": "c95762d04f5d1a902d65",
+    "url": "/f1races/static/js/main.6b2d2d41.chunk.js"
+  },
+  {
+    "revision": "d21db9805eb8e5ff17e8",
+    "url": "/f1races/static/js/2.9d9e22ea.chunk.js"
+  },
+  {
+    "revision": "c95762d04f5d1a902d65",
+    "url": "/f1races/static/css/main.e5098fbf.chunk.css"
+  },
+  {
+    "revision": "d21db9805eb8e5ff17e8",
+    "url": "/f1races/static/css/2.a9c6c121.chunk.css"
+  },
+  {
+    "revision": "9baad83b713c0b0f51b8606539a0c788",
+    "url": "/f1races/index.html"
+  }
+];
