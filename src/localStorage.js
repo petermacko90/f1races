7c5a3dd12cbb177fb279febc8339a87567b099b5
Peter Macko <petermacko90@gmail.com>
2019-05-05 19:16:15 +0200

    style fixes, remove joi, upgrade react scripts

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,4 +1,3 @@
-import Joi from 'joi';
 import { teams } from './constants';
 
 export const getCalendars = () => {
@@ -11,7 +10,6 @@ export const getCalendars = () => {
     }
     return calendars;
   } catch (error) {
-    console.error('load from localStorage error', error);
     return [];
   }
 }
@@ -21,49 +19,16 @@ export const saveRaces = (races, season) => {
     const serializedRaces = JSON.stringify(races);
     localStorage.setItem('calendar_' + season, serializedRaces);
   } catch (error) {
-    console.error('save to localStorage error', error);
     return error;
   }
 }
 
 export const loadRaces = (season) => {
   try {
-    const locationObject = Joi.object().keys({
-      lat: Joi.string(),
-      long: Joi.string(),
-      locality: Joi.string().required(),
-      country: Joi.string().required()
-    });
-    const circuitObject = Joi.object().keys({
-      circuitId: Joi.string(),
-      url: Joi.string().required(),
-      circuitName: Joi.string().required(),
-      Location: locationObject.required()
-    });
-    const raceObject = Joi.object().keys({
-      season: Joi.string().required(),
-      round: Joi.string().required(),
-      url: Joi.string().required(),
-      raceName: Joi.string().required(),
-      Circuit: circuitObject.required(),
-      date: Joi.string().required(),
-      time: Joi.string()
-    });
-    const schema = Joi.array().items(raceObject);
-
     const serializedRaces = localStorage.getItem('calendar_' + season);
     if (serializedRaces === null) return undefined;
-    const races = JSON.parse(serializedRaces);
-
-    const test = Joi.validate(races, schema);
-    if (test.error) {
-      localStorage.removeItem('calendar_' + season);
-      return undefined;
-    } else {
-      return races;
-    }
+    return JSON.parse(serializedRaces);
   } catch (error) {
-    console.error('load from localStorage error', error);
     return undefined;
   }
 }
@@ -73,40 +38,21 @@ export const saveNotifications = (notifications) => {
     const serializedNotifications = JSON.stringify(notifications);
     localStorage.setItem('notifications', serializedNotifications);
   } catch (error) {
-    console.error('save to localStorage error', error);
     return error;
   }
 }
 
 export const loadNotifications = () => {
   try {
-    const notificationObject = Joi.object().keys({
-      id: Joi.number().required(),
-      body: Joi.string().required(),
-      raceDate: Joi.date().required(),
-      notificationDate: Joi.date().required(),
-      notified: Joi.boolean().required(),
-      title: Joi.string().required()
-    });
-    const schema = Joi.array().items(notificationObject);
-
     const serializedNotifications = localStorage.getItem('notifications');
     if (serializedNotifications === null) return undefined;
-    const notifications = JSON.parse(serializedNotifications, (key, value) => {
+    return JSON.parse(serializedNotifications, (key, value) => {
       if (key === 'notificationDate' || key === 'raceDate') {
         return new Date(value);
       } else {
         return value;
       }
     });
-    
-    const test = Joi.validate(notifications, schema);
-    if (test.error) {
-      localStorage.removeItem('notifications');
-      return undefined;
-    } else {
-      return notifications;
-    }
   } catch (error) {
     return undefined;
   }
@@ -116,7 +62,7 @@ export const saveTheme = (theme) => {
   try {
     localStorage.setItem('theme', theme);
   } catch (error) {
-    console.error('save to localStorage error', error);
+    return undefined;
   }
 }
 
@@ -130,7 +76,6 @@ export const loadTheme = () => {
     }
     return 'ferrari';
   } catch (error) {
-    console.error('load from localStorage error', error);
     return 'ferrari';
   }
 }
