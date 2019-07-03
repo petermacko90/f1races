07a11f1d9af2f57167bb988d58ffd78bf1f1f419
Macko, Peter <peter.macko@accenture.com>
2019-07-03 09:39:40 +0200

    Calendars now uses hooks

--- src/components/SavedData/Calendars.js
+++ src/components/SavedData/Calendars.js
@@ -1,63 +1,54 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import './Calendars.css';
 import { getCalendars, removeCalendar } from '../../localStorage';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { toast } from 'react-toastify';
 
-class Calendars extends Component {
-  constructor() {
-    super();
-    this.state = {
-      calendars: []
-    };
-  }
+const SavedCalendars = () => {
+  const [ calendars, setCalendars ] = useState([]);
 
-  componentDidMount() {
-    const calendars = getCalendars();
-    this.setState({ calendars });
-  }
+  useEffect(() => {
+    setCalendars(getCalendars);
+  }, []);
 
-  deleteCalendar = (calendar) => () => {
-    const calendars = this.state.calendars.filter(c => c !== calendar);
+  function deleteCalendar(calendar) {
+    const filteredCalendars = calendars.filter(c => c !== calendar);
     const errror = removeCalendar(calendar);
     if (errror) {
       toast.error('Error - calendar was not deleted :(');
     } else {
-      this.setState({ calendars });
+      setCalendars(filteredCalendars);
     }
   }
 
-  render() {
-    return (
-      <Fragment>
-        <h2 className='ml3 mr3'>Saved Calendars</h2>
-        {
-          this.state.calendars.length === 0 ?
-            <p className='p3'>No saved calendars</p>
-          :
-            <ul className='ml3 mr3'>
-              {
-                this.state.calendars.map(calendar => {
-                  return (
-                    <li key={calendar}>
-                      <button
-                        className='button bg-ferrari b-ferrari mr3 mb3'
-                        title='Delete Calendar'
-                        onClick={this.deleteCalendar(calendar)}
-                      >
-                        <FontAwesomeIcon icon={faTrashAlt} />
-                      </button>
-                      <span>{calendar.slice(-4)}</span>
-                    </li>
-                  );
-                })
-              }
-            </ul>
-        }
-      </Fragment>
-    );
-  }
+  return (
+    <Fragment>
+      <h2 className='ml3 mr3'>Saved Calendars</h2>
+      {calendars.length === 0
+        ? <p className='p3'>No saved calendars</p>
+        :
+          <ul className='ml3 mr3'>
+            {
+              calendars.map(calendar => {
+                return (
+                  <li key={calendar}>
+                    <button
+                      className='button bg-ferrari b-ferrari mr3 mb3'
+                      title='Delete Calendar'
+                      onClick={() => deleteCalendar(calendar)}
+                    >
+                      <FontAwesomeIcon icon={faTrashAlt} />
+                    </button>
+                    <span>{calendar.slice(-4)}</span>
+                  </li>
+                );
+              })
+            }
+          </ul>
+      }
+    </Fragment>
+  );
 }
 
-export default Calendars;
+export default SavedCalendars;
