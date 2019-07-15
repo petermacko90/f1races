a2d9b1cd66790086cb952db100421ddcfce225ae
Peter Macko <petermacko90@gmail.com>
2019-07-15 21:41:41 +0200

    Updates

---
+++ precache-manifest.7f11c8d111f36a080ea8f1aade219007.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c1000e9b7807c2040918cfafcef08d",
+    "url": "/f1races/index.html"
+  },
+  {
+    "revision": "f061f0bc585dfdb219a6",
+    "url": "/f1races/static/css/2.f4948e68.chunk.css"
+  },
+  {
+    "revision": "4a97c452c6dc77cd3312",
+    "url": "/f1races/static/css/main.3e330701.chunk.css"
+  },
+  {
+    "revision": "f061f0bc585dfdb219a6",
+    "url": "/f1races/static/js/2.aadf7294.chunk.js"
+  },
+  {
+    "revision": "4a97c452c6dc77cd3312",
+    "url": "/f1races/static/js/main.10d45c01.chunk.js"
+  },
+  {
+    "revision": "91779dc48555e54248ab",
+    "url": "/f1races/static/js/runtime~main.d1915ec3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/f1races/precache-manifest.3a3a32348fc30bedb77f8a4e823e3a15.js"
+  "/f1races/precache-manifest.7f11c8d111f36a080ea8f1aade219007.js"
 );
 
 self.addEventListener('message', (event) => {
